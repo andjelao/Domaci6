7d891e37ae528692336e39b8de1bb456e053877b
Anđela Obradović <73198069+andjelao@users.noreply.github.com>
2020-11-04 19:27:45 +0100

    Update str03_zad23.py

--- str03_zad23.py
+++ str03_zad23.py
@@ -1,14 +1,11 @@
 x = int(input("Unesite vrijednost x: "))
 y = int(input("Unesite vrijednost y: "))
-print("Prije: ",x,y)
 def zamjena_vrijednosti(x, y):
     """
     ulazni parametri: promjenjive x i y
     mijenja mjesta vrijednostima x i y
     """
-    temp = x
-    x = y
-    y = temp
+    x, y = y, x
     print("Poslije",x, y)
 
 zamjena_vrijednosti(x, y)
