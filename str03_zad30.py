33588d0054d001ca42853e4602d1b7814d01536a
Anđela Obradović <73198069+andjelao@users.noreply.github.com>
2020-12-01 18:35:03 +0100

    Update str03_zad30.py

--- str03_zad30.py
+++ str03_zad30.py
@@ -18,14 +18,19 @@ def rastojanje(unos):
         s2 = v2 * T #predjeni put automobila2
         s1 = s1 % L
         s2 = s2 % L
-        if s1 == 0 and s2 == 0:
+        if T== 0:
+        # prije pocetka kretanja, u trenutku 0 -> rastojanje izmedju njih je jednako duzini staze jer tek treba da pocnu kretanje jedan ka drugom
             return L
         elif s1 + s2 == L:
+        # u slucaju da de u toku kretanja nadju u istoj tacki, odnosno predjeni put bude jednak duzini staze -> rastojanje je nula jer su stigli jedan do drugog
             return 0
         elif s1 + s2 < L:
+        # u slucaju da se jos uvijek nisu mimoisli, vec idu jedan ka drugom -> rastojanje je razlika duzine staze i puta koji je svaki od njih presao
             return L - (s1 + s2)
         else:
-            return abs(s1 - s2)
+        # u slucaju da su se mimoisli, oba auta su prosli odredjeni isti dio puta -> oni se ne udaljavaju, vec opet pocinju put jedan ka drugom
+        # tada je rastojanje jednako razlici duzine staze i dijela puta koji su zajedno prosli
+            return L - ((s1 + s2) % L)
        
 
 print(rastojanje(unos))
